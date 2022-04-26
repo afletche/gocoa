070cdf5232248a735ac713a321824da7c89e12f7
Andrew Fletcher <afletche@ucsd.edu>
2022-04-25 20:11:44 -0700

    started laying out theh functions

---
+++ model.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def model(x):
+    pass
+
+
+
+
+
+
+def evaluate_objective(x):
+    pass
+
+
+
+def evaluate_jacobian(x):
+    pass
+
+
+
+def evaluate_hessian(x):
+    pass
+
