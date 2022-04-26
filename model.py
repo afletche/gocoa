6acb2ee8ce9e623979042988c36b528753fc556a
Andrew Fletcher <afletche@ucsd.edu>
2022-04-25 20:27:34 -0700

    Converted to object and started writing more things

--- model.py
+++ model.py
@@ -1,24 +1,41 @@
 import numpy as np
 
 
-def model(x):
-    pass
+class Simulation:
+
+    def __init__(self):
+        self.nt = 3
+
+    
+    def preallocate_variables(self):
+        self.U = np.zeros((2*self.nt,))
 
 
 
+    '''
+    Evaluate the model
+
+    returns: [f, c, df_dx, dc_dx, ...etc. I will fill this out later]
+    '''
+    def evaluate(self, x):
+        pass
 
 
 
-def evaluate_objective(x):
-    pass
+    def simulate_dynamics(self, u):
+        pass
+
+
+    def evaluate_objective(self, u):
+        pass
 
 
 
-def evaluate_jacobian(x):
-    pass
+    def evaluate_jacobian(self, u):
+        pass
 
 
 
-def evaluate_hessian(x):
-    pass
+    def evaluate_hessian(self, u):
+        pass
 
