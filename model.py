b39333f9ddf4ffedfa17df27e4bccf22c23fb8e7
Andrew Fletcher <afletche@ucsd.edu>
2022-04-25 20:37:01 -0700

    added variable preallocation

--- model.py
+++ model.py
@@ -7,10 +7,17 @@ class Simulation:
     def __init__(self):
         self.nt = 3
 
+        self.g = 9.81
+
     
     def preallocate_variables(self):
         self.U = np.zeros((2*self.nt,))
-        
+        self.x = np.zeros((self.nt+1))
+        self.xdot = np.zeros((self.nt+1))
+        self.y = np.zeros((self.nt+1))
+        self.ydot = np.zeros((self.nt+1))
+        self.theta = np.zeros((self.nt+1))
+        self.thetadot = np.zeros((self.nt+1))
 
 
     '''
