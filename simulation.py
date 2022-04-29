fixed constraint jacobian

simulation.py
@@ -9,7 +9,7 @@ from scipy.misc import derivative
 class Simulation:
 
     def __init__(self):
-        self.nt = 20
+        self.nt = 10
 
         self.x_target = 10.
         self.xdot_target = 0.
@@ -31,7 +31,7 @@ class Simulation:
     '''
     def preallocate_variables(self):
         self.num_control_inputs = 2*self.nt
-        self.num_constraints = 4    # 2D, each dof has a final condition constraint.
+        self.num_constraints = 1    # 2D, each dof has a final condition constraint.
 
         self.u = np.zeros((2*self.nt,))
         self.x = np.zeros((self.nt+1))
@@ -71,7 +71,6 @@ class Simulation:
 
         f = self.evaluate_objective(self.u)
         c = self.evaluate_constraints()
-        # df_dx = self.evaluate_objective_gradient(self.u)
         df_dx = self.evaluate_gradient(self.u, self.lagrange_multipliers)
         dc_dx = self.evaluate_constraint_jacobian()
         d2f_dx2 = self.evaluate_objective_hessian()
@@ -120,8 +119,8 @@ class Simulation:
     '''
     Evaluates the lagrangian objective (f + lambda*c) which is equivalent to the original objective (f) because c=0
     '''
-    def evaluate_objective(self, u):
-        #return u.dot(u)
+    def evaluate_objective(self, u, rho=0.):
+        # return u.dot(u) + 1/2*rho*(self.evaluate_constraints()**2)
         return self.evaluate_constraints()
 
 
@@ -133,9 +132,9 @@ class Simulation:
 
         c = np.zeros((self.num_constraints,))
         c[0] = self.W.dot(self.x) - self.x_target
-        c[1] = self.W.dot(self.xdot) - self.xdot_target
-        c[2] = self.W.dot(self.y) - self.y_target
-        c[3] = self.W.dot(self.ydot) - self.ydot_target
+        # c[1] = self.W.dot(self.xdot) - self.xdot_target
+        # c[2] = self.W.dot(self.y) - self.y_target
+        # c[3] = self.W.dot(self.ydot) - self.ydot_target
         #c[4] = self.W.dot(self.theta) - self.theta_target
         #c[5] = self.W.dot(self.thetadot) - self.thetadot_target
 
@@ -172,7 +171,7 @@ class Simulation:
         W[-1] = 1
         pc_px = W
 
-        px_pxdot = np.tril(np.ones((self.nt, self.nt)))*self.deltat
+        px_pxdot = np.tril(np.ones((self.nt, self.nt)), -1)*self.deltat
 
         pxdotdot_pinput = np.zeros((self.nt, 2*self.nt))
         pydotdot_pinput = np.zeros((self.nt, 2*self.nt))
@@ -195,13 +194,19 @@ class Simulation:
         pydotdot_ptheta = np.diag(np.cos(self.theta[1:]))
 
         dc_du[0,:] = pc_px.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_translational + pxdotdot_ptheta.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_rotational))
-        dc_du[1,:] = pc_px.dot(px_pxdot).dot(pacceleration_pinput_translational + pxdotdot_ptheta.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_rotational))
-        dc_du[2,:] = pc_px.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_translational + pydotdot_ptheta.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_rotational))
-        dc_du[3,:] = pc_px.dot(px_pxdot).dot(pacceleration_pinput_translational + pydotdot_ptheta.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_rotational))
+        # dc_du[1,:] = pc_px.dot(px_pxdot).dot(pacceleration_pinput_translational + pxdotdot_ptheta.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_rotational))
+        # dc_du[2,:] = pc_px.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_translational + pydotdot_ptheta.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_rotational))
+        # dc_du[3,:] = pc_px.dot(px_pxdot).dot(pacceleration_pinput_translational + pydotdot_ptheta.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_rotational))
         #dc_du[4,:] = W.dot(px_pxdot).dot(px_pxdot).dot(pacceleration_pinput_rotational)
         #dc_du[5,:] = W.dot(px_pxdot).dot(pacceleration_pinput_rotational)
         
         return dc_du
+
+    def evaluate_analytic_test(self, x0):
+        self.lagrange_multipliers = np.array([x0[-1]])
+        # return self.lagrange_multipliers.dot(self.evaluate_constraint_jacobian())
+        return self.evaluate_gradient(self.u, self.lagrange_multipliers)
+        
 
 
     '''

test_file.py
@@ -3,6 +3,7 @@ import numpy as np
 from simulation import Simulation
 from optimization_framework.optimization_problem import OptimizationProblem
 from optimization_framework.optimizers.gradient_descent_optimizer import GradientDescentOptimizer
+from optimization_framework.optimizers.finite_difference import finite_difference
 
 sim1 = Simulation()
 sim1.theta[0] = np.pi/2
@@ -22,15 +23,38 @@ print(sim1.y)
 # sim1.plot_rigid_body_displacement()
 # print("hello end of file")
 
-control_optimization = OptimizationProblem()
-steepest_descent_optimizer = GradientDescentOptimizer(alpha=1e-3)
-control_optimization.set_model(model=sim1)
-control_optimization.set_optimizer(steepest_descent_optimizer)
-control_optimization.setup()
+def evaluate_fd(x0):
+    sim1.simulate_dynamics(x0[:-1])
+
+    c = np.zeros((sim1.num_constraints,))
+    c[0] = sim1.W.dot(sim1.x) - sim1.x_target
+    
+    lagrange_multipliers = np.array([x0[-1]])
+
+    # return [sim1.thetadot[-1]]
+    # return c
+    print(lagrange_multipliers.dot(c))
+    return [lagrange_multipliers.dot(c)]
+
+
+sim1.setup()
 x0 = np.ones(sim1.num_control_inputs + sim1.num_constraints,)*200.
-steepest_descent_optimizer.set_initial_guess(x0)
-print("model outputs",control_optimization.evaluate_model(x0))
+sim1.simulate_dynamics(x0[:-1])
+sim1.lagrange_multipliers = x0[-1]
+
+print('FD', finite_difference(evaluate_fd, x0))
+print('ANALYTIC', sim1.evaluate_analytic_test(x0))
+
+
+# control_optimization = OptimizationProblem()
+# steepest_descent_optimizer = GradientDescentOptimizer(alpha=1e-3)
+# control_optimization.set_model(model=sim1)
+# control_optimization.set_optimizer(steepest_descent_optimizer)
+# control_optimization.setup()
+# x0 = np.ones(sim1.num_control_inputs + sim1.num_constraints,)*200.
+# steepest_descent_optimizer.set_initial_guess(x0)
+# print("model outputs",control_optimization.evaluate_model(x0))
 #control_optimization.run(line_search='GFD', grad_norm_abs_tol=1.e-2, delta_x_abs_tol=1e-5, updating_penalty=True, max_iter=100000)
-solution = control_optimization.report(history=True)
+# solution = control_optimization.report(history=True)
 #control_optimization.plot()
-sim1.plot_rigid_body_displacement()
+# sim1.plot_rigid_body_displacement()